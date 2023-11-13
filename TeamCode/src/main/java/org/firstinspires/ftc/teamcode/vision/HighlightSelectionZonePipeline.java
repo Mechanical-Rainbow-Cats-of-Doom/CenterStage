a2b26f2825646bf8199826d185dcd1791b99033b
Patrick <psdecabooter@gmail.com>
2023-11-12 19:59:34 -0600

    im going down the pipeline

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/HighlightSelectionZonePipeline.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class HighlightSelectionZonePipeline extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        // TODO finish this
+//        PropPipeline.drawRectOnToMat(input, PropPipeline.getRects(), TsePipeline.red);
+        return input;
+    }
+}
