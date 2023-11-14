b8f258fc547c287118e4e30ce91b38270b64c33e
LoganBurfield <cthulhumoose17@gmail.com>
2023-11-13 20:16:49 -0600

    i want to give up on this it doesn't work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/DroneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/DroneLauncher.java
@@ -2,19 +2,28 @@ package org.firstinspires.ftc.teamcode.tool;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.function.BooleanSupplier;
 
 @Config
 public class DroneLauncher extends SubsystemBase {
-    public static double HELD_POS = 0.0, LAUNCHED_POS = 0.0;
+    public static double HELD_POS = 1D, LAUNCHED_POS = 0D;
     private final ServoToggle rubberBandHolder;
+    private final ButtonReader reader;
 
-    public DroneLauncher(HardwareMap hardwareMap) {
+    public DroneLauncher(HardwareMap hardwareMap, BooleanSupplier button) {
         this.rubberBandHolder = new ServoToggle(hardwareMap, "drone", HELD_POS, LAUNCHED_POS);
+        this.reader = new ButtonReader(button);
     }
 
     @Override
     public void periodic() {
+        reader.readValue();
+        if (reader.wasJustReleased()) {
+            rubberBandHolder.toggleState();
+        }
         rubberBandHolder.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/GamepadIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/GamepadIntake.java
@@ -1,21 +1,23 @@
 package org.firstinspires.ftc.teamcode.tool;
 
-import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.util.function.BooleanSupplier;
 
 public class GamepadIntake extends Intake {
-    private final ToggleButtonReader toggle;
+    private final ButtonReader toggle;
     public GamepadIntake(HardwareMap hardwareMap, BooleanSupplier toggler) {
         super(hardwareMap);
-        this.toggle = new ToggleButtonReader(toggler);
+        this.toggle = new ButtonReader(toggler);
     }
 
     @Override
     public void periodic() {
         toggle.readValue();
-        this.setState(toggle.getState());
+        if (toggle.wasJustReleased()) {
+            toggle();
+        }
         super.periodic();
     }
 }
