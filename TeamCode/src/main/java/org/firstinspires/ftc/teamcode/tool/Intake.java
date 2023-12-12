d0b1a2f4a03dc59416776467b26a8216d10ec9c8
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2023-12-11 18:52:04 -0600

    better intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/Intake.java
@@ -12,6 +12,9 @@ import org.firstinspires.ftc.teamcode.common.util.DcMotorSimpleGroup;
 
 public class Intake extends ToggleableMotor {
     public Intake(@NonNull HardwareMap hardwareMap) {
-        super(new DcMotorSimpleGroup(new Pair<>(hardwareMap.get(DcMotor.class, "intake"), false), new Pair<>(hardwareMap.get(CRServo.class, "intakeServo"), true)));
+        super(new DcMotorSimpleGroup(
+                new Pair<>(hardwareMap.get(DcMotor.class, "intake"), false),
+                new Pair<>(hardwareMap.get(CRServo.class, "intakeServo"), true)
+        ));
     }
 }
