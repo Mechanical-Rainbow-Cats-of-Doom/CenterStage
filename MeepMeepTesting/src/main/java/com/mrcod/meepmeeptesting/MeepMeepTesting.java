6e7cd0087510e9cd972a949cec5988bab3ffd908
LoganBurfield <cthulhumoose17@gmail.com>
2024-01-22 19:36:10 -0600

    meep meep2

--- MeepMeepTesting/src/main/java/com/mrcod/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/mrcod/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.mrcod.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -8,16 +9,19 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         System.setProperty("sun.java2d.opengl", "true");
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
+        final double width = 18, height = 18;
         RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
+                .setDimensions(width, height)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        bot.runAction(bot.getDrive().actionBuilder(new Pose2d(0,0,0))
-                .lineToX(30)
-                .build());
+        bot.runAction(bot.getDrive().actionBuilder(new Pose2d(-36,(height/2)-72, Math.toRadians(90)))
+//                .strafeTo(new Vector2d())
+                        .strafeTo(new Vector2d(-36, -12))
+                        .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
