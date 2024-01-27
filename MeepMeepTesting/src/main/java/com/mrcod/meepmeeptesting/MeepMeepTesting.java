bab64fc3c333594c9e6d5a5f2ef93101fdc3e568
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2024-01-26 20:15:44 -0600

    how the hell did a merge mess this up

--- MeepMeepTesting/src/main/java/com/mrcod/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/mrcod/meepmeeptesting/MeepMeepTesting.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.roadrunner.messages.meepmeeptesting;
+package com.mrcod.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Pose2dDual;
