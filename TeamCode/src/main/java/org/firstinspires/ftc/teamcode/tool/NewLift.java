e3dc9cedfd37ed02af56dbbebca4d28cc1d84b56
Patrick DeCabooter <psdecabooter@gmail.com>
2024-02-24 18:44:40 -0600

    i am dum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/NewLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tool/NewLift.java
@@ -65,8 +65,6 @@ public class NewLift extends SubsystemBase {
         enum Default implements LiftPosition {
             DOWN(0, 100, false, 0.43, 0.535, 0.9, false, true,
                     Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.VERY_EARLY, Time.VERY_EARLY, Time.NORMAL, Time.VERY_EARLY, Time.VERY_EARLY),
-
-
             T_LEFT_LOW(800, 100, true, 0.76, 0.76, 0.35, true, true,
                     Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY, Default.DOWN),
             T_LEFT_MEDIUM(1400, T_LEFT_LOW.liftTicksTwo, T_LEFT_LOW.armOut, T_LEFT_LOW.armRoll, T_LEFT_LOW.carriageRoll, T_LEFT_LOW.armLength, T_LEFT_LOW.clawOpen, T_LEFT_LOW.retractArm,
@@ -90,25 +88,25 @@ public class NewLift extends SubsystemBase {
 
             // NO WORK
             A_VLOW_MIDDLEDUMP_RIGHT_LEAN(0, 100, true, 0.7, 0.535, 1, false, true,
-                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY),
+                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, null, Time.VERY_EARLY),
             // NO WORK
             A_VLOW_MIDDLEDUMP_LEFT_LEAN(0, 100, true, 0.7, 0.535, 1, false, true,
-                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY),
+                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, null, Time.VERY_EARLY),
             A_MIDDLE_VLOW_LEFTDUMP(200, 100, true, 0.25, 0.3, 1, false, true,
-                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY),
+                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, null, Time.VERY_EARLY),
             A_MIDDLE_VLOW_RIGHTDUMP(100, 100, true, 0.5, 0.75, 1, false, true,
-                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY),
+                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, null, Time.VERY_EARLY),
 
             A_LEFT_VLOW_LEFTDUMP(100, 100, true, 0.5, 0.75, 0.35, false, true,
-                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY),
+                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, null, Time.VERY_EARLY),
             A_LEFT_VLOW_MIDDLEDUMP(300, 100, true, 0.8, 1, 0.35, false, true,
-                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.VERY_LATE_BUTTON, Time.VERY_EARLY),
+                    Time.NORMAL, Time.VERY_EARLY, Time.EARLY, Time.NORMAL, Time.NORMAL, Time.NORMAL, null, Time.VERY_EARLY),
 
 
             A_MEDIUM(2000, 100, true, 0.43, 0.535, 1, true, true,
-                     Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.EARLY, Time.LATE, Time.VERY_LATE_BUTTON, Time.EARLY),
+                     Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.EARLY, Time.LATE, null, Time.EARLY),
             A_HIGH(3000, 100, true, 0.43, 0.535, 1, true, true,
-                   Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.EARLY, Time.LATE, Time.VERY_LATE_BUTTON, Time.EARLY);
+                   Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.NORMAL, Time.EARLY, Time.LATE, null, Time.EARLY);
 
             private final int liftTicks;
             private final int liftTicksTwo;
@@ -489,7 +487,7 @@ public class NewLift extends SubsystemBase {
 
         @Override
         public Time getLiftMoveTime() {
-            return Time.NORMAL;
+            return null;
         }
 
         @Override
@@ -701,7 +699,7 @@ public class NewLift extends SubsystemBase {
                 if(!usesTime(position, LiftPosition.Time.VERY_LATE_BUTTON)) {
                     state = State.AT_POSITION;
                 } else {
-                    if (!toolGamepad.getButton(GamepadKeys.Button.Y)) {
+                    if (toolGamepad != null && !toolGamepad.getButton(GamepadKeys.Button.Y)) {
                         break;
                     }
                     state = State.VERY_LATE_BUTTON_MOVE;
@@ -919,9 +917,6 @@ public class NewLift extends SubsystemBase {
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             if(!started) {
-                if(carriageClawServo.getPosition() == (open ? CLAW_OPEN_POSITION : CLAW_CLOSED_POSITION)) {
-                    return true;
-                }
                 setClaw(open);
                 started = true;
                 time.reset();
